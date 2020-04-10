47dc454151187122e3f247f13f6716ceab5df34e
Alex Bechanko <chanko08@gmail.com>
2020-04-10 19:22:52 -0400

    updated predictions to filter empty patterns

--- animal_crossing/turnips/__init__.py
+++ animal_crossing/turnips/__init__.py
@@ -3,9 +3,10 @@ from animal_crossing.turnips import patterns
 from animal_crossing.turnips.prediction import Prediction
 from animal_crossing.turnips.price import PriceRange
 
-def predict(buy_price, sell_prices):
+def predict(buy_price, sell_prices, show_empty_patterns=False):
     predictions = []
     for pattern_name, pattern in patterns.ALL_PATTERNS.items():
         p = Prediction.with_pattern(buy_price, pattern_name.value, pattern).with_sell_prices(sell_prices).consolidate().replace_ranges_with_data(sell_prices)
-        predictions.append(p)
+        if show_empty_patterns or p:
+            predictions.append(p)
     return predictions
